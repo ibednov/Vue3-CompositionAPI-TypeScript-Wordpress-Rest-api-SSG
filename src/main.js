16e6126d436295bc336b8dc5fb51df5193b4be25
Alepsios <ibednov@live.ru>
2022-10-22 05:32:09 +0300

    feat: add plugins (flowbite-vue & @saeris/vue-spinners)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { i18n } from './plugins/i18n.js'
 import router from './router'
 import 'flowbite';
+import { VueSpinners } from '@saeris/vue-spinners'
 
 import './style.css'
 import './assets/css/tailwind.css'
@@ -11,4 +12,5 @@ import App from './App.vue'
 createApp(App)
 .use(i18n)
 .use(router)
+.use(VueSpinners)
 .mount('#app')
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,9 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    // "./src/**/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
+    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+
   ],
   theme: {
     extend: {},
@@ -16,7 +17,6 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
     require('flowbite/plugin')
-
   ],
   darkMode: 'media', // or 'media' or 'class'
 }
