c53f70bead53eb20eb411f199ce8ff2010ab2a40
Alepsios <ibednov@live.ru>
2022-10-22 03:36:35 +0300

    fix: edit ContactPage.vue

--- src/views/pages/ContactsPage.vue
+++ src/views/pages/ContactsPage.vue
@@ -6,8 +6,6 @@
 </script>
 
 <template>
-
-
     <div
         class="container mx-auto flex flex-col md:flex-row rounded-xl p-4 md:px-10 md:py-7 bg-purple-300 dark:bg-gradient-to-br dark:from-fuchsia-900 dark:to-purple-900">
         <div class="basis-1/2 order-2 md:order-first py-6 flex flex-col gap-4 self-center md:px-10">
@@ -18,7 +16,6 @@
                 Designer & full-stack developer
             </div>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-4 py-4">
-
                 <a href="https://www.instagram.com/ibednov/" target="_blank" type="button"
                     class="flex flex-row gap-4 justify-center content-center text-white dark:text-black bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-xl text-sm px-5 py-5 text-center mr-2 mb-2">
                     <div>
@@ -47,7 +44,6 @@
                         Behance
                     </div>
                 </a>
-
                 <a href="https://t.me/alepsios" target="_blank" type="button"
                     class="flex flex-row gap-4 justify-center content-center text-white dark:text-black bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-xl text-sm px-5 py-5 text-center mr-2 mb-2">
                     <div>
@@ -101,13 +97,10 @@
                         WhatsApp
                     </div>
                 </a> -->
-
             </div>
-
         </div>
         <div class="basis-1/2">
             <img class="rounded-xl" src="http://127.0.0.1:8080/wp-content/uploads/2021/02/low_IMG_022.webp" alt="">
         </div>
-
     </div>
 </template>
