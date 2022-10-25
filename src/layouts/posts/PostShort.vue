875f3f6edd097d3208cf1d153dbea401edf1986e
Alepsios <ibednov@live.ru>
2022-10-26 02:55:38 +0300

    ref: delete second v-if

--- src/layouts/posts/PostShort.vue
+++ src/layouts/posts/PostShort.vue
@@ -1,20 +1,16 @@
 <script setup>
 import PostShortType01 from './PostShortTypes/PostShortType01.vue';
-import PostShortType04 from './PostShortTypes/PostShortType04.vue';
-import PostShortType03 from './PostShortTypes/PostShortType03.vue';
 import PostShortType02 from './PostShortTypes/PostShortType02.vue';
+import PostShortType03 from './PostShortTypes/PostShortType03.vue';
+import PostShortType04 from './PostShortTypes/PostShortType04.vue';
+
 const props = defineProps({
     post: Object,
 });
-
 </script>
-
 <template>
-    <div v-if="post.acf.disable_title === false">
-        <PostShortType01 v-if="post.acf.short_type === 'short_type_1'" :post="post" />
-        <PostShortType02 v-else-if="post.acf.short_type === 'short_type_2'" :post="post" />
-        <PostShortType03 v-else="post.acf.short_type === 'short_type_3'" :post="post" />
-    </div>
-
+    <PostShortType01 v-if="post.acf.short_type === 'short_type_1'" :post="post" />
+    <PostShortType02 v-else-if="post.acf.short_type === 'short_type_2'" :post="post" />
+    <PostShortType03 v-else-if="post.acf.short_type === 'short_type_3'" :post="post" />
     <PostShortType04 v-else :post="post" />
 </template>
--- src/layouts/posts/PostShortTypes/PostShortType04.vue
+++ src/layouts/posts/PostShortTypes/PostShortType04.vue
@@ -11,12 +11,10 @@ const snippet = computed(() => {
 </script>
 
 <template>
-    <div>
-        short type 4
-    </div>
-    <router-link :to="{ name: 'PostFull', params: { id: post.id, post: post } }" 
-    class="flex flex-col justify-start  max-h-full" id="short_type_4">
-        <img class="rounded-xl max-h-80 max-w-20 justify-self-center" :src="post.featured_image_urls.thumbnail[0]"
-            :alt="post.title.rendered" />
+    <router-link :to="{ name: 'PostFull', params: { id: post.id, post: post } }"
+        :class="post.acf.short_bg ? post.acf.short_bg : 'bg-stone-200 dark:bg-stone-900' "
+        :id="post.acf.short_type">
+        <img class="object-contain rounded-xl max-h-80 max-w-20 justify-self-center"
+            :src="post.featured_image_urls.medium_large[0]" :alt="post.title.rendered" />
     </router-link>
 </template>
