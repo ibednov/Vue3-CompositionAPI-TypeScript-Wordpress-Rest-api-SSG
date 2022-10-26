0846f8a0ff037886a9f1f3e84315150192f4075f
Alepsios <ibednov@live.ru>
2022-10-26 17:27:21 +0300

    ref: replace flowbite-vue with flowbite
    - complete this commit "wip / need to fix: start to replace flowbite-vue with flowbite"
    - delete flowbite-vue package  and dependences.
    - replace txt with lang variables

--- src/views/pages/HomePage.vue
+++ src/views/pages/HomePage.vue
@@ -1,178 +1,136 @@
 <script setup>
 import Category from "./CategoryById.vue";
 
-import { ref } from 'vue'
-import { Tabs, Tab } from 'flowbite-vue'
+// const tabs = [
+//   {
+//     id: 'uiux',
+//     triggerEl: document.querySelector('#profile-tab-example'),
+//     targetEl: document.querySelector('#uiux-example')
+//   },
+//   {
+//     id: 'brand',
+//     triggerEl: document.querySelector('#brand-tab-example'),
+//     targetEl: document.querySelector('#brand-example')
+//   },
+//   {
+//     id: 'dev',
+//     triggerEl: document.querySelector('#dev-tab'),
+//     targetEl: document.querySelector('#dev')
+//   },
+//   {
+//     id: 'gamedev',
+//     triggerEl: document.querySelector('#gamedev-tab-example'),
+//     targetEl: document.querySelector('#gamedev-example')
+//   },
+//   {
+//     id: 'blog',
+//     triggerEl: document.querySelector('#blog-tab-example'),
+//     targetEl: document.querySelector('#blog-example')
+//   }
 
-const activeTab = ref('uiux')
-
-// // create an array of objects with the id, trigger element (eg. button), and the content element
-// const tabElements = [
-//     {
-//         id: 'profile',
-//         triggerEl: document.querySelector('#profile-tab-example'),
-//         targetEl: document.querySelector('#profile-example')
-//     },
-//     {
-//         id: 'dashboard',
-//         triggerEl: document.querySelector('#dashboard-tab-example'),
-//         targetEl: document.querySelector('#dashboard-example')
-//     },
-//     {
-//         id: 'settings',
-//         triggerEl: document.querySelector('#settings-tab-example'),
-//         targetEl: document.querySelector('#settings-example')
-//     },
-//     {
-//         id: 'contacts',
-//         triggerEl: document.querySelector('#contacts-tab-example'),
-//         targetEl: document.querySelector('#contacts-example')
-//     }
 // ];
-
-// // options with default values
-// const options = {
-//     defaultTabId: 'settings',
-//     activeClasses: 'text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-400 border-blue-600 dark:border-blue-500',
-//     inactiveClasses: 'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
-//     onShow: () => {
-//         console.log('tab is shown');
-//     }
-// };
-
-// /*
-// * tabElements: array of tab objects
-// * options: optional
-// */
-// const tabs = new Tabs(tabElements, options);
-// // shows another tab element
-// tabs.show('dashboard');
-
-// // get the tab object based on ID
-// tabs.getTab('contacts')
-
-// // get the current active tab object
-// tabs.getActiveTab()
 </script>
 
 <template>
-  <!-- 
-  <div>
+  <main class="container mx-auto">
+    <div class="mb-4 border-b 0">
+      <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="main_tabs"
+        data-tabs-toggle="#main_categories" role="tablist">
+        <li class="mr-2" role="presentation">
+          <button
+            class="flex flex-row gap-4 p-4 rounded-t-lg border-b-2 text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500"
+            id="uiux-tab" data-tabs-target="#uiux" type="button" role="tab" aria-controls="uiux" aria-selected="true">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z">
+              </path>
+            </svg>
+            {{ $t('categories.uiux') }}
+          </button>
+        </li>
+        <li class="mr-2" role="presentation">
+          <button
+            class="flex flex-row gap-4 p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700"
+            id="brand-tab" data-tabs-target="#brand" type="button" role="tab" aria-controls="brand"
+            aria-selected="false">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z">
+              </path>
+            </svg>
+            {{ $t('categories.brand') }}
+          </button>
+        </li>
+        <li class="mr-2" role="presentation">
+          <button
+            class="flex flex-row gap-4 p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700"
+            id="dev-tab" data-tabs-target="#dev" type="button" role="tab" aria-controls="dev" aria-selected="false">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path>
+            </svg>
+            {{ $t('categories.dev') }}
+          </button>
+        </li>
+        <li class="mr-2" role="presentation">
+          <button
+            class="flex flex-row gap-4 p-4 rounded-t-lg cursor-not-allowed border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700"
+            id="gamedev-tab" data-tabs-target="#gamedev" type="button" role="tab" aria-controls="gamedev"
+            aria-selected="false" disabled>
+            <svg class="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-gamepad-variant-outline"
+              viewBox="0 0 24 24">
+              <path
+                d="M6,9H8V11H10V13H8V15H6V13H4V11H6V9M18.5,9A1.5,1.5 0 0,1 20,10.5A1.5,1.5 0 0,1 18.5,12A1.5,1.5 0 0,1 17,10.5A1.5,1.5 0 0,1 18.5,9M15.5,12A1.5,1.5 0 0,1 17,13.5A1.5,1.5 0 0,1 15.5,15A1.5,1.5 0 0,1 14,13.5A1.5,1.5 0 0,1 15.5,12M17,5A7,7 0 0,1 24,12A7,7 0 0,1 17,19C15.04,19 13.27,18.2 12,16.9C10.73,18.2 8.96,19 7,19A7,7 0 0,1 0,12A7,7 0 0,1 7,5H17M7,7A5,5 0 0,0 2,12A5,5 0 0,0 7,17C8.64,17 10.09,16.21 11,15H13C13.91,16.21 15.36,17 17,17A5,5 0 0,0 22,12A5,5 0 0,0 17,7H7Z" />
+            </svg>
 
-    <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
-      <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400" id="tabExample"
-        role="tablist">
-        <li class="mr-2" role="presentation">
-          <button
-            class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            id="profile-tab-example" type="button" role="tab" aria-controls="profile-example"
-            aria-selected="false">Profile</button>
-        </li>
-        <li class="mr-2" role="presentation">
-          <button
-            class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            id="dashboard-tab-example" type="button" role="tab" aria-controls="dashboard-example"
-            aria-selected="false">Dashboard</button>
-        </li>
-        <li class="mr-2" role="presentation">
-          <button
-            class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            id="settings-tab-example" type="button" role="tab" aria-controls="settings-example"
-            aria-selected="false">Settings</button>
+            {{ $t('categories.gamedev') }}
+          </button>
         </li>
         <li role="presentation">
           <button
-            class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            id="contacts-tab-example" type="button" role="tab" aria-controls="contacts-example"
-            aria-selected="false">Contacts</button>
+            class="flex flex-row gap-4 p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700"
+            id="blog-tab" data-tabs-target="#blog" type="button" role="tab" aria-controls="blog" aria-selected="false">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+              </path>
+            </svg>
+            {{ $t('categories.blog') }}
+          </button>
         </li>
       </ul>
     </div>
-    <div id="tabContentExample">
-      <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="profile-example" role="tabpanel"
-        aria-labelledby="profile-tab-example">
-        <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
-            class="font-medium text-gray-800 dark:text-white">Profile tab's associated content</strong>. Clicking
-          another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control
-          the content visibility and styling.</p>
-      </div>
-      <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="dashboard-example" role="tabpanel"
-        aria-labelledby="dashboard-tab-example">
-        <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
-            class="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</strong>. Clicking
-          another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control
-          the content visibility and styling.</p>
-      </div>
-      <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="settings-example" role="tabpanel"
-        aria-labelledby="settings-tab-example">
-        <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
-            class="font-medium text-gray-800 dark:text-white">Settings tab's associated content</strong>. Clicking
-          another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control
-          the content visibility and styling.</p>
-      </div>
-      <div class="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800" id="contacts-example" role="tabpanel"
-        aria-labelledby="contacts-tab-example">
-        <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong
-            class="font-medium text-gray-800 dark:text-white">Contacts tab's associated content</strong>. Clicking
-          another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control
-          the content visibility and styling.</p>
-      </div>
-    </div>
 
-  </div> -->
-
-  <main class="container mx-auto p-4">
-    <tabs variant="pills" v-model="activeTab" class="py-5 justify-center content-center">
-      <tab name="uiux" title="UI/UX design">
+    <div id="main_categories">
+      <div class="p-4 " id="uiux" role="tabpanel" aria-labelledby="uiux-tab">
         <KeepAlive>
           <Category :cat="14" />
         </KeepAlive>
-      </tab>
-      <tab name="brand" title="Brand & marketing design">
+      </div>
+      <div class="hidden p-4 " id="brand" role="tabpanel" aria-labelledby="brand-tab">
         <KeepAlive>
           <Category :cat="87" />
         </KeepAlive>
-      </tab>
-      <tab name="front" title="Frontend & backend development">
+      </div>
+      <div class="hidden p-4 " id="dev" role="tabpanel" aria-labelledby="dev-tab">
         <KeepAlive>
           <Category :cat="13" />
         </KeepAlive>
-      </tab>
-      <tab name="gamedev" title="GameDev" :disabled="true">
-
-      </tab>
-      <tab name="blog" title="Blog">
+      </div>
+      <div class="hidden p-4 " id="gamedev" role="tabpanel" aria-labelledby="gamedev-tab">
+        {{ $t('categories.gamedev') }}
+      </div>
+      <div class="hidden p-4 " id="blog" role="tabpanel" aria-labelledby="blog-tab">
         <KeepAlive>
           <Category :cat="2" />
         </KeepAlive>
-      </tab>
-    </tabs>
+      </div>
+    </div>
   </main>
-
-  <!-- <section class="flex flex-row">
-    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-        d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z">
-      </path>
-    </svg>
-    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-        d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z">
-      </path>
-    </svg>
-    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-        d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
-      </path>
-    </svg>
-    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
-      </path>
-    </svg>
-  </section> -->
-
-
-
 </template>
 
