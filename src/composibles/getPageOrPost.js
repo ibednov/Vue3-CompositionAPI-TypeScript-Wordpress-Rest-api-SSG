e31441219715e50efc0c78a940b80e80137fdcd7
Alepsios <ibednov@live.ru>
2022-11-01 17:20:38 +0300

    Update getPageOrPost.js

--- src/composibles/getPageOrPost.js
+++ src/composibles/getPageOrPost.js
@@ -1,20 +1,20 @@
 import { ref } from "vue";
-import axios from 'axios'
+import axios from "axios";
 
-import '../plugins/wp-stackable.js'
+import "../plugins/wp-stackable.js";
 
-const url = "http://127.0.0.1:8080/wp-json/wp/v2/"
+const url = "http://127.0.0.1:8080/wp-json/wp/v2/";
 const getPage = (type, id) => {
-    const page = ref([]);
-    const load = async () => {
-        try {
-            let { data } = await axios(url + type + '/' + id)
-            page.value = data
-        } catch (error) {
-            console.log(error)
-        }
+  const page = ref([]);
+  const load = async () => {
+    try {
+      let { data } = await axios(url + type + "/" + id);
+      page.value = data;
+    } catch (error) {
+      console.log(error);
     }
-    return { page, load }
-}
+  };
+  return { page, load };
+};
 
-export default getPage
+export default getPage;
