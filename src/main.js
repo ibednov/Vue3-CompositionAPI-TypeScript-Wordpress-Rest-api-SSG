fb56de2d25169caa079994e1b2d915f9ef5fa37b
Alepsios <ibednov@live.ru>
2022-11-02 00:39:39 +0300

    WIP: feat: add PWA for vite

--- src/main.js
+++ src/main.js
@@ -9,4 +9,11 @@ import "./style.css";
 
 import App from "./App.vue";
 
+import { registerSW } from "virtual:pwa-register";
+
+if ("serviceWorker" in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW();
+}
+
 createApp(App).use(i18n).use(router).use(store).mount("#app");
--- vite.config.cjs
+++ vite.config.cjs
@@ -1,20 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-// import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // VitePWA({
-    //   registerType: "autoUpdate",
-    //   devOptions: {
-    //     enabled: true,
-    //     type: "module",
-    //   },
-    //   injectRegister: "script",
-    // }),
+    VitePWA({
+      registerType: "autoUpdate",
+      // devOptions: {
+      //   enabled: true,
+      //   type: "module",
+      // },
+      // injectRegister: "script",
+    }),
     eslintPlugin(),
   ],
 });
