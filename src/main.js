c7430adb7cc598b984b80a8e5687243a984f7639
Alepsios <ibednov@live.ru>
2022-11-01 20:46:44 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-import router from './router'
-import store from './store'
+import router from "./router";
+import store from "./store";
 
-import { i18n } from './plugins/i18n.js'
+import { i18n } from "./plugins/i18n.js";
 
-import './style.css'
+import "./style.css";
 
-import App from './App.vue'
+import App from "./App.vue";
 
-
-createApp(App)
-.use(i18n)
-.use(router)
-.use(store)
-.mount('#app')
+createApp(App).use(i18n).use(router).use(store).mount("#app");
