5f87a235f45c382fe16c7a72e49061bcb7a42d4b
Alepsios <ibednov@live.ru>
2022-11-15 05:09:35 +0300

    Update vite.config.cjs
    - add alias

--- vite.config.cjs
+++ vite.config.cjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 import eslintPlugin from "vite-plugin-eslint";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,4 +18,9 @@ export default defineConfig({
     }),
     eslintPlugin(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
