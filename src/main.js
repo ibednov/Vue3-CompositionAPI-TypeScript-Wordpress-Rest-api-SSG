a33253bcffa214f1082bafd02cda88a4a4fac6f4
Alepsios <ibednov@live.ru>
2022-11-26 01:29:25 +0300

    delete SW

--- src/main.js
+++ src/main.js
@@ -5,15 +5,9 @@ import router from "./router";
 import store from "./store";
 
 import { i18n } from "./plugins/i18n.js";
-import { registerSW } from "virtual:pwa-register";
 
 import "./style.css";
 import App from "./App.vue";
-
-if ("serviceWorker" in navigator) {
-  // && !/localhost/.test(window.location)) {
-  registerSW();
-}
 
 const pinia = createPinia();
 
--- vite.config.cjs
+++ vite.config.cjs
@@ -1,23 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { VitePWA } from "vite-plugin-pwa";
 import eslintPlugin from "vite-plugin-eslint";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      registerType: "autoUpdate",
-      // devOptions: {
-      //   enabled: true,
-      //   type: "module",
-      // },
-      // injectRegister: "script",
-    }),
-    eslintPlugin(),
-  ],
+  plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
