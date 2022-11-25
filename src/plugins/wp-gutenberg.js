fd50c6119b753cdb48d2dc28600e58a001b78111
Alepsios <ibednov@live.ru>
2022-11-26 01:48:32 +0300

    fix: ref plugin's init

---
+++ src/plugins/wp-gutenberg.js
@@ -0,0 +1,8 @@
+import "https://api.alexbednov.work/wp-includes/js/admin-bar.min.js";
+// import 'https://api.alexbednov.work/wp-includes/js/hoverintent-js.min.js'
+// import 'https://api.alexbednov.work/wp-includes/js/imagesloaded.min.js'
+// import 'https://api.alexbednov.work/wp-includes/js/masonry.min.js'
+import "https://api.alexbednov.work/wp-includes/js/jquery/jquery.min.js";
+
+import "https://api.alexbednov.work/wp-content/plugins/kadence-blocks/dist/vendor/slick.min.js";
+import "https://api.alexbednov.work/wp-content/plugins/kadence-blocks/dist/kt-slick-init.js";
--- src/plugins/wp-stackable.js
+++ src/plugins/wp-stackable.js
@@ -1,26 +1,2 @@
-// // import 'http://127.0.0.1:8080/wp-content/plugins/stackable-ultimate-gutenberg-blocks-premium/dist/frontend_blocks.js'
-// import 'http://127.0.0.1:8080/wp-content/plugins/stackable-ultimate-gutenberg-blocks/dist/deprecated/frontend_blocks_deprecated_v2.js'
-
-// const stackableCss = await import('http://127.0.0.1:8080/wp-content/plugins/stackable-ultimate-gutenberg-blocks/dist/frontend_blocks.css', {
-//     assert: { type: 'css' }
-// });
-
-// const stackableResponsiveCss = await import('http://127.0.0.1:8080/wp-content/plugins/stackable-ultimate-gutenberg-blocks/dist/frontend_blocks_responsive.css', {
-//     assert: { type: 'css' }
-// });
-
-// const stackableCssDeprecated = await
-//     import('http://127.0.0.1:8080/wp-content/plugins/stackable-ultimate-gutenberg-blocks/dist/deprecated/frontend_blocks_deprecated.css', {
-//         assert: { type: 'css' }
-//     });
-// const stackableCssDeprecatedv2 = await
-//     import('http://127.0.0.1:8080/wp-content/plugins/stackable-ultimate-gutenberg-blocks/dist/deprecated/frontend_blocks_deprecated_v2.css', {
-//         assert: { type: 'css' }
-//     });
-
-// document.adoptedStyleSheets = [
-//     stackableResponsiveCss.default,
-//     stackableCssDeprecated.default,
-//     stackableCssDeprecatedv2.default,
-//     stackableCss.default
-// ]
+// import 'https://api.alexbednov.work/wp-content/plugins/stackable-ultimate-gutenberg-blocks-premium/dist/frontend_blocks.js'
+import "https://api.alexbednov.work/wp-content/plugins/stackable-ultimate-gutenberg-blocks/dist/deprecated/frontend_blocks_deprecated_v2.js";
