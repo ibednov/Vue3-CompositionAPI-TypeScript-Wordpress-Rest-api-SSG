455c0026289f0257ed4d40189455a8ff3761340b
Alepsios <ibednov@live.ru>
2022-11-26 04:16:08 +0300

    fix: wp plugins

--- src/plugins/wp-stackable.js
+++ src/plugins/wp-stackable.js
@@ -1,2 +1,2 @@
 // import 'https://api.alexbednov.work/wp-content/plugins/stackable-ultimate-gutenberg-blocks-premium/dist/frontend_blocks.js'
-import "https://api.alexbednov.work/wp-content/plugins/stackable-ultimate-gutenberg-blocks/dist/deprecated/frontend_blocks_deprecated_v2.js";
+// import "https://api.alexbednov.work/wp-content/plugins/stackable-ultimate-gutenberg-blocks/dist/deprecated/frontend_blocks_deprecated_v2.js";
