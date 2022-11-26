0caad6a0b91577ee144756dd947888b2bf98a58e
Alepsios <ibednov@live.ru>
2022-11-26 04:10:08 +0300

    fix: upd. with absolute links

--- src/composibles/getPageOrPost.js
+++ src/composibles/getPageOrPost.js
@@ -1,7 +1,7 @@
 import { ref } from "vue";
 import axios from "axios";
 
-import "../plugins/wp-stackable.js";
+import "@/plugins/wp-stackable.js";
 
 const url = "https://api.alexbednov.work/wp-json/wp/v2/";
 const getPage = (type, id) => {
--- src/composibles/getPost.js
+++ src/composibles/getPost.js
@@ -1,6 +1,7 @@
 import { ref } from "vue";
 import axios from "axios";
-import "../plugins/wp-stackable.js";
+
+import "@/plugins/wp-stackable.js";
 
 const url = "https://api.alexbednov.work/wp-json/wp/v2/posts/";
 const getPost = (id) => {
