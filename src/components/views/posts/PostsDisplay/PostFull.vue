5fb32a2d28eb797a2f491f301414e4e1c53ac230
Alepsios <ibednov@live.ru>
2022-11-26 04:25:04 +0300

    fix: replace txt with Spinner

--- src/components/views/posts/PostsDisplay/PostFull.vue
+++ src/components/views/posts/PostsDisplay/PostFull.vue
@@ -3,6 +3,7 @@ import { useRoute } from "vue-router";
 import getPageOrPost from "@/composibles/getPageOrPost";
 import PostHeader from "./PostFullDisplay/PostHeader.vue";
 import PostBody from "./PostFullDisplay/PostBody.vue";
+import SpinnerLoader from "@/components/ui/layouts/SmallComponents/SpinnerLoader.vue";
 
 const route = useRoute();
 const { page, load: loadGetPageOrPost } = getPageOrPost(
@@ -23,7 +24,9 @@ const props = defineProps({
     <div class="flex flex-col">
       <PostHeader v-if="page.type === 'post'" :page="page" />
       <PostHeader v-else-if="page.type === 'team'" :page="page" />
-      <div v-else>What can I do?</div>
+      <div v-else>
+        <SpinnerLoader />
+      </div>
     </div>
     <PostBody :page="page" />
   </article>
