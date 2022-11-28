fc838d8be5e9bba1c1b97fd0651ab6c30c49742a
Alepsios <ibednov@live.ru>
2022-11-28 18:46:05 +0300

    upd. conf's

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,18 @@
 module.exports = {
   env: {
-    node: true,
+    es6: true,
+    browser: true,
+    es2021: true,
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   parser: "vue-eslint-parser",
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
   rules: {
+    'prettier/prettier': 'error'
     // override/add rules settings here, such as:
     // "vue/no-unused-vars": "error",
     // Enable vue/script-setup-uses-vars rule
