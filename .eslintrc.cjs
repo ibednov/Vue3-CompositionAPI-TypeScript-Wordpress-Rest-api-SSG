3d649e2d8ad4651f90958248c59ad6bceb075f55
Alepsios <ibednov@live.ru>
2022-11-30 01:33:47 +0300

    fix: eslint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
-  parser: "vue-eslint-parser",
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  parser: 'vue-eslint-parser',
   plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
-    'prettier/prettier': 'error'
+    'prettier/prettier': 'error',
     // override/add rules settings here, such as:
     // "vue/no-unused-vars": "error",
     // Enable vue/script-setup-uses-vars rule
