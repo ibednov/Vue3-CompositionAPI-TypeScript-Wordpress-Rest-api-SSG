0fa2df8d4244243f2a3af0fb27160674be5dfef4
Alepsios <ibednov@live.ru>
2022-11-30 04:51:11 +0300

    feat: replace with DropDown component

--- src/components/ui/layouts/SmallComponents/LangSwitcher.vue
+++ src/components/ui/layouts/SmallComponents/LangSwitcher.vue
@@ -1,4 +1,8 @@
 <script setup>
+import DropDown from '@/components/ui/dropdown/DropDown.vue';
+import FlagRU from '@/components/ui/flags/FlagRU.vue';
+import FlagEN from '@/components/ui/flags/FlagEN.vue';
+
 import { useLangStore } from '@/pinia/lang';
 
 const lang = useLangStore().lang; // pinia store
@@ -13,186 +17,41 @@ const handleChangeLocaleEn = () => {
 </script>
 
 <template>
-  <div class="flex items-center md:order-2">
-    <button
-      v-show="store.state.locale === 'en'"
-      type="button"
-      data-dropdown-toggle="language-dropdown-menu-ru"
-      class="inline-flex justify-center items-center p-2 text-sm text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
-    >
-      <svg
-        class="mr-2 w-5 h-5 rounded-full"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        viewBox="0 0 3900 3900"
-      >
-        <path fill="#b22234" d="M0 0h7410v3900H0z"></path>
-        <path
-          d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
-          stroke="#fff"
-          stroke-width="300"
-        ></path>
-        <path fill="#3c3b6e" d="M0 0h2964v2100H0z"></path>
-        <g fill="#fff">
-          <g id="d">
-            <g id="c">
-              <g id="e">
-                <g id="b">
-                  <path
-                    id="a"
-                    d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"
-                  ></path>
-                  <use xlink:href="#a" y="420"></use>
-                  <use xlink:href="#a" y="840"></use>
-                  <use xlink:href="#a" y="1260"></use>
-                </g>
-                <use xlink:href="#a" y="1680"></use>
-              </g>
-              <use xlink:href="#b" x="247" y="210"></use>
-            </g>
-            <use xlink:href="#c" x="494"></use>
-          </g>
-          <use xlink:href="#d" x="988"></use>
-          <use xlink:href="#c" x="1976"></use>
-          <use xlink:href="#e" x="2470"></use>
-        </g>
-      </svg>
-      <span class="hidden sm:block">
-        {{ $t('langs.en') }}
-      </span>
-    </button>
-    <button
-      v-show="store.state.locale === 'ru'"
-      type="button"
-      data-dropdown-toggle="language-dropdown-menu-en"
-      class="inline-flex justify-center items-center p-2 text-sm text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
-    >
-      <svg
-        id="flag-icon-css-ru"
-        class="mr-2 w-5 h-5 rounded-full"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-      >
-        <g fill-rule="evenodd" stroke-width="1pt">
-          <path fill="#fff" d="M0 0h512v512H0z" />
-          <path fill="#0039a6" d="M0 170.7h512V512H0z" />
-          <path fill="#d52b1e" d="M0 341.3h512V512H0z" />
-        </g>
-      </svg>
-      <span class="hidden sm:block">
-        {{ $t('langs.ru') }}
-      </span>
-    </button>
+  <DropDown
+    v-if="lang.currentLang == 'en'"
+    @handle-change="handleChangeLocaleRu"
+  >
+    <template #text>
+      {{ $t('langs.en') }}
+    </template>
+    <template #icon>
+      <FlagEN />
+    </template>
 
-    <!-- Dropdown -->
-    <div
-      id="language-dropdown-menu-en"
-      data-popper-reference-hidden=""
-      data-popper-escaped=""
-      data-popper-placement="bottom"
-      class="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
-      style="
-        position: absolute;
-        inset: 0px auto auto 0px;
-        margin: 0px;
-        transform: translate(0px, 12653px);
-      "
-    >
-      <ul class="py-1" role="none">
-        <li>
-          <button
-            class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-            role="menuitem"
-            @click="handleChangeLocaleEn"
-          >
-            <div class="inline-flex items-center">
-              <svg
-                class="mr-2 w-5 h-5 rounded-full"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                viewBox="0 0 3900 3900"
-              >
-                <path fill="#b22234" d="M0 0h7410v3900H0z"></path>
-                <path
-                  d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
-                  stroke="#fff"
-                  stroke-width="300"
-                ></path>
-                <path fill="#3c3b6e" d="M0 0h2964v2100H0z"></path>
-                <g fill="#fff">
-                  <g id="d">
-                    <g id="c">
-                      <g id="e">
-                        <g id="b">
-                          <path
-                            id="a"
-                            d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"
-                          ></path>
-                          <use xlink:href="#a" y="420"></use>
-                          <use xlink:href="#a" y="840"></use>
-                          <use xlink:href="#a" y="1260"></use>
-                        </g>
-                        <use xlink:href="#a" y="1680"></use>
-                      </g>
-                      <use xlink:href="#b" x="247" y="210"></use>
-                    </g>
-                    <use xlink:href="#c" x="494"></use>
-                  </g>
-                  <use xlink:href="#d" x="988"></use>
-                  <use xlink:href="#c" x="1976"></use>
-                  <use xlink:href="#e" x="2470"></use>
-                </g>
-              </svg>
-              {{ $t('langs.en') }}
-            </div>
-          </button>
-        </li>
-      </ul>
-    </div>
+    <template #drop_text>
+      {{ $t('langs.ru') }}
+    </template>
+    <template #drop_icon>
+      <FlagRU />
+    </template>
+  </DropDown>
 
-    <!-- Dropdown -->
-    <div
-      id="language-dropdown-menu-ru"
-      data-popper-reference-hidden=""
-      data-popper-escaped=""
-      data-popper-placement="bottom"
-      class="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
-      style="
-        position: absolute;
-        inset: 0px auto auto 0px;
-        margin: 0px;
-        transform: translate(0px, 12653px);
-      "
-    >
-      <ul class="py-1" role="none">
-        <li>
-          <button
-            class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-            role="menuitem"
-            @click="handleChangeLocaleRu"
-          >
-            <div class="inline-flex items-center">
-              <svg
-                id="flag-icon-css-ru"
-                class="mr-2 w-5 h-5 rounded-full"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <g fill-rule="evenodd" stroke-width="1pt">
-                  <path fill="#fff" d="M0 0h512v512H0z" />
-                  <path fill="#0039a6" d="M0 170.7h512V512H0z" />
-                  <path fill="#d52b1e" d="M0 341.3h512V512H0z" />
-                </g>
-              </svg>
-              {{ $t('langs.ru') }}
-            </div>
-          </button>
-        </li>
-      </ul>
-    </div>
+  <DropDown
+    v-if="lang.currentLang == 'ru'"
+    @handle-change="handleChangeLocaleEn"
+  >
+    <template #text>
+      {{ $t('langs.ru') }}
+    </template>
+    <template #icon>
+      <FlagRU />
+    </template>
 
-
+    <template #drop_text>
+      {{ $t('langs.en') }}
+    </template>
+    <template #drop_icon>
+      <FlagEN />
+    </template>
+  </DropDown>
 </template>
