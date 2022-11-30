db209c8c202829e889124898893b8a491d5b2b87
Alepsios <ibednov@live.ru>
2022-11-30 04:35:37 +0300

    feat: create DropDown component

---
+++ src/components/ui/dropdown/DropDown.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { reactive, ref } from 'vue';
+
+const data = ref({
+  isVisible: false,
+});
+
+const emit = defineEmits(['handleChange']);
+
+const handleChangeLocale = ref(() => {
+  toggleVisibility();
+  emit('handleChange');
+});
+
+const toggleVisibility = reactive(() => {
+  try {
+    data.value.isVisible = !data.value.isVisible;
+    console.log('try to drop');
+  } catch (error) {
+    console.log('drop error');
+  }
+});
+</script>
+
+<template>
+  <div class="flex flex-col items-center md:order-2">
+    <button
+      type="button"
+      class="inline-flex justify-center items-center px-2 py-4 gap-4 text-sm text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+      @click="toggleVisibility"
+    >
+      <slot name="icon" />
+      <span class="hidden sm:block">
+        <slot name="text" />
+      </span>
+    </button>
+
+    <!-- Dropdown -->
+    <div
+      v-show="data.isVisible"
+      class="absolute z-50 -top-16 md:top-16 my-4 text-base w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
+    >
+      <button
+        type="button"
+        class="inline-flex justify-center items-center px-2 py-4 gap-4 text-sm text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+        @click="handleChangeLocale"
+      >
+        <slot name="drop_icon" />
+
+        <slot name="drop_text" />
+      </button>
+    </div>
+  </div>
+</template>
