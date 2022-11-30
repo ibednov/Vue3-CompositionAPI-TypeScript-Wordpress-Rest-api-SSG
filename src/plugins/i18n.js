2fc4c000d80b9f4d85595bbd209c763c06642848
Alepsios <ibednov@live.ru>
2022-11-30 04:30:40 +0300

    ref: update i18n plugin with pinia

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,20 +1,16 @@
 import { createI18n } from 'vue-i18n';
 import ru from '../lang/ru.json';
+import en from '../lang/en.json';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+const pinia = createPinia();
+createApp().use(pinia);
+import { useLangStore } from '@/pinia/lang';
 
 export const i18n = createI18n({
-  legacy: true,
   fallbackLocale: 'ru', // set fallback locale
-  locale: 'ru', // set locale
-  messages: { ru }, // set locale messages
+  locale: useLangStore().lang.currentLang, // set locale
+  messages: { ru, en }, // set locale messages
 });
-
-export async function loadMessages(locale) {
-  console.log(locale);
-  if (Object.keys(i18n.global.getLocaleMessage(locale)).length === 0) {
-    const messages = await import('../lang/' + locale /* @vite-ignore */);
-    i18n.global.setLocaleMessage(locale, messages);
-  }
-  if (i18n.global.locale !== locale) {
-    i18n.global.locale = locale;
-  }
-}
