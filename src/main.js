9a7f2400d74d70fd2daa7bf687ebaacbacda90a3
Alepsios <ibednov@live.ru>
2022-11-30 04:29:37 +0300

    feat: add subscribe method to main.js

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ import '@/assets/style.css';
 import App from '@/components/App.vue';
 
 const pinia = createPinia();
+import { useLangStore } from '@/pinia/lang';
 
 const Vue = createApp(App);
+
 Vue.use(i18n).use(router).use(pinia).mount('#app');
+
+useLangStore().$subscribe((_, state) => {
+  console.info(state.lang);
+  i18n.global.locale = state.lang.currentLang;
+});
