741367b90491a8bec9789059a5b42843dcd35f50
Alepsios <ibednov@live.ru>
2022-12-01 16:21:29 +0300

    fix: Post by Category padding

--- src/components/views/posts/PostsByCategory.vue
+++ src/components/views/posts/PostsByCategory.vue
@@ -16,7 +16,7 @@ loadPosts();
 <template>
   <div
     v-if="posts.length"
-    class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-x-4 gap-y-6 rounded-xl p-10 bg-gradient-to-r from-cyan-100 to-indigo-100 dark:from-cyan-900 dark:to-indigo-900"
+    class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-x-4 gap-y-6 rounded-xl bg-gradient-to-r from-cyan-100 to-indigo-100 dark:from-cyan-900 dark:to-indigo-900 md:p-6"
   >
     <PostShort
       v-for="post in posts"
