d416d13e7e290636af8c8a575f5f1ac3b3e23d9a
Alepsios <ibednov@live.ru>
2022-12-07 06:40:21 +0300

    ref: Tabs component
    - edit Tabs List component
    - and related index.vue
    -- replace svg icons with UnoCss
    - minor layout fix

--- src/components/ui/Tabs/TabsList.vue
+++ src/components/ui/Tabs/TabsList.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-  import { ref, provide, useSlots } from 'vue';
-  const slots: object = ref(useSlots())
-  const tabsArray: object = ref(slots.value.default().map((tab) => tab.props))
-  const selectedTitle: object = ref(tabsArray.value[0].name)
-  provide('selectedTitle', selectedTitle)
-  const {t} = useI18n()
+import { ref, provide, useSlots } from 'vue';
+const slots: object = ref(useSlots())
+const tabsArray: object = ref(slots.value.default().map((tab) => tab.props))
+const selectedTitle: object = ref(tabsArray.value[0].name)
+provide('selectedTitle', selectedTitle)
+const { t } = useI18n()
 </script>
 
 <template>
   <div class="flex flex-col md:flex-row gap-4 justify-center mt-4 py-6">
     <button v-for="item in tabsArray" :key="item.name"
-      class="grow px-8 py-4 rounded-xl text-center hover:bg-violet-200 dark:hover:bg-indigo-800 flex flex-row gap-x-3 justify-center"
+      class="grow px-8 py-4 items-center rounded-xl text-center hover:bg-violet-200 dark:hover:bg-indigo-800 flex flex-row gap-x-3 justify-center"
       :class="{
         'bg-gradient-to-r from-cyan-200 to-violet-200 text-black dark:from-cyan-900 dark:to-violet-900 dark:text-white':
           selectedTitle == item.name,
@@ -20,7 +20,7 @@
           item.disabled,
       }" :disabled="item.disabled" @click="selectedTitle = item.name" @click.prevent="item.disabled">
       <!-- eslint-disable-next-line vue/no-v-html -->
-      <div v-html="item.icon" />
+      <div :class="item.icon" class="text-2xl" />
       <div class="font-light font-xl">
         {{ t(item.title) }}
       </div>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,90 +11,19 @@ const { t } = useI18n()
 <template>
   <main class="container mx-auto">
     <TabsList>
-      <TabSingle :title="'categories.uiux-design'" name="uiux" icon="
-      <svg
-            class='w-6 h-6'
-            fill='none'
-            stroke='currentColor'
-            viewBox='0 0 24 24'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              stroke-width='2'
-              d='M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z'
-            ></path>
-          </svg>
-      ">
-
+      <TabSingle :title="'categories.uiux-design'" name="uiux" icon="i-mingcute:file-code-line">
         <PostsByCategory :cat="14" />
-
       </TabSingle>
-      <TabSingle :title="'categories.brand-marketing-design'" name="brand" icon="<svg
-      class='w-6 h-6'
-      fill='none'
-      stroke='currentColor'
-      viewBox='0 0 24 24'
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <path
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='2'
-        d='M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z'
-      ></path>
-    </svg>">
+      <TabSingle :title="'categories.brand-marketing-design'" name="brand" icon="i-mingcute:fire-line">
         <PostsByCategory :cat="87" />
       </TabSingle>
-      <TabSingle :title="'categories.development'" name="dev" icon="<svg
-      class='w-6 h-6'
-      fill='none'
-      stroke='currentColor'
-      viewBox='0 0 24 24'
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <path
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='2'
-        d='M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4'
-      ></path>
-    </svg>">
-
+      <TabSingle :title="'categories.development'" name="dev" icon="i-mingcute:code-line">
         <PostsByCategory :cat="13" />
-
       </TabSingle>
-      <TabSingle :title="'categories.gamedev'" name="gamedev" disabled="true" icon=" <svg
-      id='mdi-gamepad-variant-outline'
-      class='w-6 h-6'
-      fill='currentColor'
-      xmlns='http://www.w3.org/2000/svg'
-      xmlns:xlink='http://www.w3.org/1999/xlink'
-      version='1.1'
-      viewBox='0 0 24 24'
-    >
-      <path
-        d='M6,9H8V11H10V13H8V15H6V13H4V11H6V9M18.5,9A1.5,1.5 0 0,1 20,10.5A1.5,1.5 0 0,1 18.5,12A1.5,1.5 0 0,1 17,10.5A1.5,1.5 0 0,1 18.5,9M15.5,12A1.5,1.5 0 0,1 17,13.5A1.5,1.5 0 0,1 15.5,15A1.5,1.5 0 0,1 14,13.5A1.5,1.5 0 0,1 15.5,12M17,5A7,7 0 0,1 24,12A7,7 0 0,1 17,19C15.04,19 13.27,18.2 12,16.9C10.73,18.2 8.96,19 7,19A7,7 0 0,1 0,12A7,7 0 0,1 7,5H17M7,7A5,5 0 0,0 2,12A5,5 0 0,0 7,17C8.64,17 10.09,16.21 11,15H13C13.91,16.21 15.36,17 17,17A5,5 0 0,0 22,12A5,5 0 0,0 17,7H7Z'
-      />
-    </svg>">
+      <TabSingle :title="'categories.gamedev'" name="gamedev" disabled="true" icon="i-mingcute:game-1-line">
       </TabSingle>
-      <TabSingle :title="'categories.blog'" name="blog" icon="<svg
-      class='w-6 h-6'
-      fill='none'
-      stroke='currentColor'
-      viewBox='0 0 24 24'
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <path
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        stroke-width='2'
-        d='M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z'
-      ></path>
-    </svg>">
+      <TabSingle :title="'categories.blog'" name="blog" icon="i-mingcute:edit-2-line">
         <PostsByCategory :cat="2" />
-
       </TabSingle>
     </TabsList>
   </main>
